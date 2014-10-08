73772c4d37988b58ac37d1ed7ff79af45e287c8a
anschutz <artemstar@hotmail.com>
2014-10-08 14:59:13 +0300

    First test for the queue

--- test/CoreTest.js
+++ test/CoreTest.js
@@ -21,6 +21,7 @@ describe('Ar-Drone FlightCore module', function(){
         //TODO beforeEach separate require core.
     });
 
+
     describe('state manager', function(){
 
         it('should have get/set functions', function(){
@@ -53,5 +54,13 @@ describe('Ar-Drone FlightCore module', function(){
             expect(FC.$flightState).to.exist;
             expect(FC.$flightState.get()).to.equal('off');
         });
-    })
+    });
+
+    describe('drone flight queue', function(){
+      it('should successfully instantiate the queue and its data should be an empty array.',function(){
+        var cQ = new FC._flightQueue();
+        expect(cQ).to.not.be.undefined;
+        expect(cQ.data).to.be.empty;
+      });
+    });
 })
