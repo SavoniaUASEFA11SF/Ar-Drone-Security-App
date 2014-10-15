ee62f5136a26f441875c45915d08b8d5b115d3fb
Petr Dyachikhin <Lordink@Petrs-MacBook-Pro.local>
2014-10-15 13:50:23 +0300

    Added empty refresh loop to stateManager

--- FlightCore.js
+++ FlightCore.js
@@ -29,12 +29,16 @@
 
     //Initialize drone variables and connection here.
     function init() {
-        var arDrone = {}, control = {};
+        var arDrone = {},
+            control = {},
+            error  = null;
+
         try {
             arDrone = require('ar-drone');
             control = arDrone.createUdpControl();
         } catch (err) {
             console.log(" _init error: " + err.message);
+            error = err; 
         }
 
         //ref object
@@ -43,6 +47,9 @@
 
         $flightControl.$arDrone = arDrone;
         $flightControl.$udpController = control;
+
+        if(!error)
+          $flightControl.refreshIntervalId = setInterval($flightControl.refreshLoop, 1000);
     }
 
     Node.init = init;
@@ -52,6 +59,8 @@
 
         //Allowed states:
         this.states = ['off', 'takingOff', 'airborne', 'landing'];
+        //future container for the refresh interval id, which is needed to stop the refresh loop
+        this.refreshIntervalId = null;
 
         if (state === undefined || (this.states.indexOf(state) === -1))
             state = 'off'; else
@@ -64,6 +73,11 @@
             if (this.states.indexOf(newState) !== -1)
                 this.currentState = newState; else
                 throw new Error("Unknown state!");
+        };
+
+        //setInterval'd loop, invoked from the init function. Is capable of killing itself, if needed.
+        this.refreshLoop = function(){
+             
         };
     }
 
