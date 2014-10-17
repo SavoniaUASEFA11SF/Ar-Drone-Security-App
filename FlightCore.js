a6be61a153d2d1b95e75e836975ac50b944db4ac
Petr Dyachikhin <Lordink@Petrs-MacBook-Pro.local>
2014-10-17 14:03:57 +0300

    Discovered unknown error. While including Core, it throws Unknown Command. Why the fuck??

--- FlightCore.js
+++ FlightCore.js
@@ -14,9 +14,7 @@
     var Node = module.exports;
 
     //Internal flight controlling methods
-    var $flightData = {},
-        stateControl = new stateManager('off'),
-        $flightQueue = new flightQueue();
+    var $flightData = {}, stateControl = new stateManager('off'), $flightQueue = new flightQueue();
 
     Node._flightData = $flightData;
     Node.$flightState = stateControl;
@@ -29,32 +27,31 @@
     // fly("forward", 500)
     // fly({ angle: 120, duration: 500});
     // TODO: rebuild into callback
-    function fly(direction, delay)  {
-      if (typeof direction === "string") {
-        if ((direction !== "Forward") && (direction !== "Backwards") && (direction != "Left") && (direction != "Right")) {
-            return false;
-        }
-        if(delay === undefined)
-            $flightQueue.add(direction);
-        else
-            $flightQueue.add(direction, delay);
-        return true;
+    function fly(direction, delay) {
+        if (typeof direction === "string") {
+            if ((direction !== "Forward") && (direction !== "Backwards") && (direction != "Left") && (direction != "Right")) {
+                return false;
+            }
+            if (delay === undefined)
+                $flightQueue.add(direction);
+            else
+                $flightQueue.add(direction, delay);
+            return true;
         }
         //TODO: check the fact that angle and duration do exist! Otherwise, you get a thow TypeError, which is not what we want.
-        if (!isNaN(direction.angle) && !isNaN(direction.duration))  {
+        if (!isNaN(direction.angle) && !isNaN(direction.duration)) {
             $flightQueue.add("Custom Direction", direction);
             return true;
-        }else{
+        } else {
             return false;
         }
     }
+
     Node.fly = fly;
 
     //Initialize drone variables and connection here.
     function init() {
-        var arDrone = {},
-            control = {},
-            error  = null;
+        var arDrone = {}, control = {}, error = null;
 
         try {
             arDrone = require('ar-drone');
@@ -71,7 +68,7 @@
         $flightData.$arDrone = arDrone;
         $flightData.$udpController = control;
 
-        if(error)
+        if (error)
             return false;
 
         stateControl.refreshIntervalId = setInterval(stateControl.refreshLoop, 1000);
@@ -91,36 +88,35 @@
         this.refreshIntervalId = null;
 
         if (state === undefined || (this.states.indexOf(state) === -1))
-            this.currentState = 'off';
-        else
+            this.currentState = 'off'; else
             this.currentState = state;
         ///Will be instantiated for each object, but we have singleton, so no problem there.
         this.get = function () {
             return this.currentState;
         };
         this.set = function (newState) {
-            if (this.states.indexOf(newState) === -1){
+            if (this.states.indexOf(newState) === -1) {
                 throw new Error("Unknown state!");
             }
 
             //Translate the state information into ref and pcmd object
-           this.currentState = newState;
+            this.currentState = newState;
 
-           $flightData.$ref = {}; //TODO
-           $flightData.$pcmd = {}; //TODO
+            $flightData.$ref = {}; //TODO
+            $flightData.$pcmd = {}; //TODO
 
-           this.processDroneData();
+            this.processDroneData();
         };
-       //Send our pcmd and ref packages to drone controller
-        this.processDroneData = function(){
-           $flightData.$udpController.ref($flightData.$ref);
-           $flightData.$udpController.pcmd($flightData.$pcmd);
+        //Send our pcmd and ref packages to drone controller
+        this.processDroneData = function () {
+            $flightData.$udpController.ref($flightData.$ref);
+            $flightData.$udpController.pcmd($flightData.$pcmd);
 
-           $flightData.$udpController.flush();
+            $flightData.$udpController.flush();
         };
 
         //setInterval'd loop, invoked from the init function. Is capable of killing itself, if needed.
-        this.refreshLoop = function(){
+        this.refreshLoop = function () {
 
         };
     }
@@ -132,34 +128,40 @@
 
     function flightQueue() {
         this.data = [];
+        // comman types:
+        // @type 0 - essential command. Affects the whole
+        // @type 1 -  direction command. Is supposed to have angle variable.
         this.commands = [
-            { name: "Take Off",         delay: 3000 },
-            { name: "Land",             delay: 3000 },
-            { name: "Forward",          delay: -1},
-            { name: "Backwards",        delay: -1},
-            { name: "Left",             delay: -1},
-            { name: "Right",            delay: -1},
+            { name: "Take Off", type: 0, delay: 3000 },
+            { name: "Land", type: 0, delay: 3000 },
+            { name: "Forward", type: 1, angle: 0, delay: -1},
+            { name: "Backwards", type: 1, angle: 180, delay: -1},
+            { name: "Left", type: 1, angle: 270, delay: -1},
+            { name: "Right", type: 1, angle: 90, delay: -1}
         ];
         this.add = function (commandName, delay) {
-          var foundCommand = false;
+            var foundCommand = false;
+
             for (var i = 0; i < this.commands.length; i++) {
+                // if we found a command in the commands list
                 if (this.commands[i].name == commandName) {
-                  if (delay !== undefined)  {
-                    this.data.push({ name: commandName, delay: delay });
-                  }
-                  else
-                    this.data.push(this.commands[i]);
+                    foundCommand = true;
+                    // if it supplies a custom delay
+                    if (delay !== undefined)
+                        this.data.push({ name: commandName, delay: delay });
+                    else  // if it doesn't, just push it to the commands array.
+                        this.data.push(this.commands[i]);
 
                     break;
                 }
             }
-          if (!foundCommand)  {
-            throw new Error ('Unknown Command!');
-          }
+ //TODO: analyze the type of command before pushing it, and if essential - make a sophisticated decision.
+            if (!foundCommand)
+                throw new Error('Unknown Command!');
         };
 
         this.remove = function (commandName) {
-          var foundCommand = false;
+            var foundCommand = false;
             for (var i = 0; i < this.commands.length; i++) {
                 if (this.commands[i].name == commandName) {
                     this.data.splice(i, 1);
@@ -167,10 +169,11 @@
                     break;
                 }
             }
-            if (!foundCommand)  {
-              return false; }
-              else {
-                return true; }
+            if (!foundCommand) {
+                return false;
+            } else {
+                return true;
+            }
         };
 
     }
